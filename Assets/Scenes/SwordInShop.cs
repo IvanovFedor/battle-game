a4bdc174ad11d64eb5296b76a68e7c373281b61c
IvanovFedor <107537543+IvanovFedor@users.noreply.github.com>
2022-10-22 14:07:34 +0300

    Юху

--- Assets/Scenes/SwordInShop.cs
+++ Assets/Scenes/SwordInShop.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class SwordInShop : MonoBehaviour
 {
+
+    private void Start()
+    {
+        if(PlayerPrefs.GetInt("Weapon") == 3)
+            PlayerPrefs.SetInt("Weapon", 1);
+    }
+
     public void OnMouseDown()
     {
         PlayerPrefs.SetInt("Weapon", 1);
